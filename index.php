ddbaa0c6ce71e90de014bf8082877f8f881a227e
Wiley Lau <henloboii@protonmail.ch>
2021-11-30 21:48:57 +0800

    tiferet: add food id validation tool
    * also do some rearrangement and logic change
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- index.php
+++ index.php
@@ -4,19 +4,29 @@
 
 <h1>Project Tiferet is up and running! </h1>
 
+<h2>Barcode Generator</h2>
 <?php
-
 /* import start */
 require 'vendor/autoload.php';
 
 /* variable start */
 $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
-$foodid = strtotime("now"); 
+$baseid = strtotime("now");
+$locker = 9; 
+$foodid = $baseid * $locker * 1014 * 29;
 
 /* real bug trigger */
-echo "Generated Food Identity code is $foodid <br> <br> <br>";
 echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($foodid, $generator::TYPE_CODE_128)) . '">';
+echo "<br> <br>";
+echo "Timestamp is $baseid<br> <br> Food ID is $foodid <br> <br>";
 ?>
+
+<h2>Food ID Validator</h2>
+<form action="valid.php" method="get">
+Timestamp: <input type="text" name="base"><br>
+Food ID: <input type="text" name="food"><br>
+<input type="submit">
+</form>
 
 </body>
 </html>
---
+++ valid.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<?php
+/* var start */
+$locker = $_GET["food"] / 1014 / $_GET["base"] / 29;
+
+echo "locker is $locker";
+?>
+
+</body>
+</html>
