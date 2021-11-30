cd094612563c8e62a17aeb531f28aeeafad629e3
Wiley Lau <henloboii@protonmail.ch>
2021-11-30 21:51:25 +0800

    tiferet: add (in)valid locker detection
    * we will have max 10 lockers for now, even if we are only using 1 for showcase
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- valid.php
+++ valid.php
@@ -5,7 +5,13 @@
 /* var start */
 $locker = $_GET["food"] / 1014 / $_GET["base"] / 29;
 
-echo "locker is $locker";
+if ($locker > 10){
+        /* locker will be invalid as we are currently max 10 */
+        echo "Invalid Food ID detected <br>";
+} else {
+        echo "Valid Food ID detected and food is stored in locker $locker";
+}
+
 ?>
 
 </body>
