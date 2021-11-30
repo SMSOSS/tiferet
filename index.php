b9a06e63c3850111619b8c76e35d6e3db752cdea
Wiley Lau <henloboii@protonmail.ch>
2021-11-30 21:06:04 +0800

    tiferet: initial push
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Project Tiferet is up and running! </h1>
+<?php
+
+$foodid = strtotime("now"); 
+
+echo "Generated Food Identity code is $foodid";
+?>
+
+</body>
+</html>
