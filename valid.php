dfe2883c39e29a8af1aae3b8edbc5265c3782496
Wiley Lau <henloboii@protonmail.ch>
2021-11-30 21:56:22 +0800

    tiferet: return value as invalid if it contains decimals
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- valid.php
+++ valid.php
@@ -10,6 +10,8 @@ $locker = $_GET["food"] / 1014 / $_GET["base"] / 29;
 if ($locker > 10){
         /* locker will be invalid as we are currently max 10 */
         echo "Invalid Food ID detected <br>";
+} elseif($locker !== 0.00){
+        echo "Invalid Food ID detected <br>";
 } else {
         echo "Valid Food ID detected and food is stored in locker $locker";
 }
