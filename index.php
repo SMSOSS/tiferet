2e215ec77d6a5f6bbc07ba3121b8dcd961d5d323
Wiley Lau <henloboii@protonmail.ch>
2021-11-30 21:15:15 +0800

    tiferet: add barcode gen for Food ID code
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 <body>
 
 <h1>Project Tiferet is up and running! </h1>
+
 <?php
 
+/* import start */
+require 'vendor/autoload.php';
+
+/* variable start */
+$generator = new Picqer\Barcode\BarcodeGeneratorPNG();
 $foodid = strtotime("now"); 
 
-echo "Generated Food Identity code is $foodid";
+/* real bug trigger */
+echo "Generated Food Identity code is $foodid <br> <br> <br>";
+echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($foodid, $generator::TYPE_CODE_128)) . '">';
 ?>
 
 </body>
