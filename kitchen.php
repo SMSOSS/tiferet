455ac4ded0d5147892c39d74c45fed1d1a9e451f
Wiley Lau <henloboii@protonmail.ch>
2021-12-01 21:29:51 +0800

    tiferet: show names of pending orders
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- kitchen.php
+++ kitchen.php
@@ -13,6 +13,7 @@ db_open();
 /* start function and variable */
 $count = db_query("SELECT COUNT(baseid) as total FROM food;");
 $pending = db_query("SELECT foodname FROM food");
+$prows = db_num_rows($pending);
 
 $countr=db_fetch_array($count);
 $rcount=$countr['total'];
@@ -21,8 +22,14 @@ if ($rcount == 0){
         echo "No pending orders. Hooray!";
 } else {
         echo "There are $rcount pending orders. <br> <br>";
+        echo "They are: <br> <br>";
+        if ($prows > 0) {
+                while($row = $pending->fetch_assoc()) {
+                        echo $row['foodname'];
+                        echo "<br>";
+                }
+        }
 }
-
 ?>
 
 </body>
