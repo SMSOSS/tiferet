2086fa4d98d0e1b8fc966e46456746034aafb6df
Wiley Lau <henloboii@protonmail.ch>
2021-12-01 18:01:28 +0800

    tiferet: force sudo on setup
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get install composer php-imagick phpmyadmin php-mbstring mysql-server -y
+sudo apt-get install composer php-imagick phpmyadmin php-mbstring mysql-server -y
 composer require picqer/php-barcode-generator
