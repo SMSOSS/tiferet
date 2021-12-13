0bd53e302db859a7e1f363765054f68936d5fcac
Wiley Lau <henloboii@protonmail.ch>
2021-12-13 16:16:04 +0800

    tiferet: thank user for ordering after taking food
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- user.php
+++ user.php
@@ -18,7 +18,7 @@ db_open();
 
 /* variable start */
 $timestamp = $_GET["baseid"];
-$cook = db_query("SELECT iscooked, isdeliver, password FROM food WHERE baseid='$timestamp';");
+$cook = db_query("SELECT iscooked, isdeliver, istaken, password FROM food WHERE baseid='$timestamp';");
 $prows = db_num_rows($cook);
 $food = $_GET["food"];
 
@@ -27,11 +27,14 @@ if ($prows > 0) {
                 $iscook = $row['iscooked'];
                 $isdeliver = $row['isdeliver'];
                 $password = $row['password'];
+		$istaken = $row['istaken'];
         }
 }
 
 if ($iscook == 0) {
         echo "Your $food is being prepared. Please be patient.";
+} elseif ($istaken == 1) {
+	echo "Please enjoy your meal";
 } elseif ($isdeliver == 1) {
         echo "Your $food is ready.";
         echo "<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$password&choe=UTF-8%22%20title=%22your%20order%20is%20ready%22' />";
