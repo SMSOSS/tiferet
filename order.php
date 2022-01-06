5505f88992a45abf07e53e0c77b7dcc33170fb11
Wiley Lau <henloboii@protonmail.ch>
2022-01-06 16:41:44 +0800

    tiferet: order: massive code rewrite
    * now menu depends on database, restaurant can set their own menu by database
    * redirect to confirmorder with new values
    * cleanup old redirection code, remove unneeded conditionals, comments and simplify procedure.
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- order.php
+++ order.php
@@ -1,58 +1,78 @@
 <!DOCTYPE html>
-<html>
+<head>
+<style>
+h1 {
+        text-align: center;
+}
+
+h3 {
+        text-align: center;
+}
+</style>
+</head>
+
 <body>
 <?php
-
-/* include */
 include("vars.php");
-        if (isset($_POST["submitdone"]) || $_GET['food'] != "" ) {
-                global $food;
-                $food = $_POST["fname"];
-                $qlocker = db_query("SELECT lockerid FROM lockerdata WHERE isoccupy=0 LIMIT 1");
-                $qcount = db_num_rows($qlocker);
-                if ($qcount > 0) {
-                        while($row = $qlocker->fetch_assoc()) {
-                                $locker = 0;
-                                $locker = $row['lockerid'];
-                        }
-                } else {
-                        // echo "<script>location.replace('sad.php?food=$food');</script>";
-                }
-                db_query("INSERT INTO food (locker, baseid, foodid, iscooked, isdeliver, istaken, foodname, password) VALUES ($locker, $baseid, $foodid, 0, 0, 0, '$food', '$pass')");
-                db_query("UPDATE lockerdata SET isoccupy='1' WHERE lockerid='$locker'");
-                echo "<script>location.replace('user.php?food=$food&baseid=$baseid');</script>";
-        } else {
-?>
-<title>order food</title>
-<h2>order food here :3</h2>
-
-<?php
-        $qlocker = db_query("SELECT lockerid FROM lockerdata WHERE isoccupy=1;");
+if (isset($_POST["food"])){
+        $foodname = $_POST["food"];
+        $qlocker = db_query("SELECT lockerid FROM lockerdata WHERE isoccupy=0 LIMIT 1");
         $qcount = db_num_rows($qlocker);
         if ($qcount > 0) {
                 while($row = $qlocker->fetch_assoc()) {
-                $locker = 0;
-                $locker = $row['lockerid'];
+                        $locker = 0;
+                        $locker = $row['lockerid'];
+                }
         }
+        db_query("INSERT INTO food (locker, baseid, foodid, iscooked, isdeliver, istaken, foodname, password) VALUES ($locker, $baseid, $foodid, 0, 0, 0, '$foodname', '$pass')");
+        db_query("UPDATE lockerdata SET isoccupy='1' WHERE lockerid='$locker'");
+        echo "<script>location.replace('user.php?food=$foodname&baseid=$baseid');</script>";
+
+} else {
+
+$qlocker = db_query("SELECT lockerid FROM lockerdata WHERE isoccupy=1;");
+$qcount = db_num_rows($qlocker);
+if ($qcount > 0) {
+        while($row = $qlocker->fetch_assoc()) {
+        $locker = 0;
+        $locker = $row['lockerid'];
 }
-        if ($qcount >= $lcount) {
-                echo "There are no more lockers. Ordering stopped.";
+}
+if ($qcount >= $lcount) {
+        echo "There are no more lockers. Ordering stopped.";
+        echo "<br>";
+} else {
+?>
+
+<h1>
+        Menu: 
+</h1>
+
+<h3>
+<?php
+$qfood = db_query("SELECT food FROM menu WHERE soldout=0;");
+$rfood = db_num_rows($qfood);
+
+if ($rfood > 0) {
+        $base = 0;
+        while($row = $qfood->fetch_assoc()) {
+                $food = 0;
+                $food = $row['food'];
+                echo "$food";
+                echo "<form method='post'>";
+                echo "<input type='hidden' name='food' value='" . $row['food'] . "'>";
+                $temp = "<input type='submit' value='Order!' name='order'>";
+                echo $temp;
+                echo "</form>";
                 echo "<br>";
+                
         }
-        else {
-?>
-<form method="post">
-<input type="text" name="fname"><br>
-<input type="submit" name="submitdone">
-<?php
-        }
+
+}
+
+}
+
+}
 ?>
 
-</form>
-
-</body>
-</html>
-
-<?php
-        }
-?>
+</h3>
