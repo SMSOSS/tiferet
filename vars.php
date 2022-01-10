ad054f0167b9fc8e67c5d012d969e9f207e862cb
Wiley Lau <henloboii@protonmail.ch>
2022-01-10 17:28:19 +0800

    [DNM] tiferet: hardcode foodid with a dummy value
    * was set for uniqueness at first, but as far as the code goes, we aren't even able to use it.
    * instead for passing a big ton of data (very very long character), just hardcode it as 1 for now.
    * Will consider actually re-using the uniqueness concept or dropping the variable treewide in the future.
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- vars.php
+++ vars.php
@@ -14,7 +14,7 @@ $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
 $baseid = strtotime("now");
 // $locker = 9; 
 $pass = mt_rand(1000,9999);
-$foodid = $baseid * 29;
+$foodid = 1;
 $lcount = 2; // number of lockers
 
 /* real bug trigger. save all data into db */
