0bf5fab5ea378e7aae9bafee6ba0ffc93bc0b9c0
Wiley Lau <henloboii@protonmail.ch>
2022-01-14 09:06:08 +0800

    tiferet: add auto-scaling for mobile devices
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- kitchen.php
+++ kitchen.php
@@ -4,6 +4,7 @@
 <head>
 <title>Kitchen system</title>
 <meta http-equiv="refresh" content="10">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
 <style>
 
--- user.php
+++ user.php
@@ -3,6 +3,7 @@
 
 <head>
 <meta http-equiv="refresh" content="10">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
 <style>
 
