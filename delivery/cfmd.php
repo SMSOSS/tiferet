f7f3b359a4417895ce24b435273cd56a32aaa950
Wiley Lau <henloboii@protonmail.ch>
2022-01-18 15:49:01 +0800

    delivery: cfmd: run login check
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- delivery/cfmd.php
+++ delivery/cfmd.php
@@ -1,98 +1,117 @@
 <html>
 
 <head>
-<meta http-equiv="refresh" content="10">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-<style>
+    <meta http-equiv="refresh" content="10">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <style>
+        @font-face {
+            font-family: HarmonyBold;
+            src: url('/fonts/bold.ttf');
+        }
 
-@font-face { font-family: HarmonyBold; src: url('/fonts/bold.ttf'); }
-@font-face { font-family: HarmonyReg; src: url('/fonts/regular.ttf'); }
-@font-face { font-family: HarmonyLight; src: url('/fonts/light.ttf'); }
-h1 {
-        text-align: center;
-        font-family: HarmonyBold
-}
+        @font-face {
+            font-family: HarmonyReg;
+            src: url('/fonts/regular.ttf');
+        }
 
-h3 {
-        text-align: center;
-        font-family: HarmonyLight
-}
+        @font-face {
+            font-family: HarmonyLight;
+            src: url('/fonts/light.ttf');
+        }
 
-body {
-background-color: #DBF9FC;
-}
+        h1 {
+            text-align: center;
+            font-family: HarmonyBold
+        }
 
-input[type=submit] {
-    padding:5px 15px;
-    background:#DBF9FC;
-    border:1px solid black;
-    cursor:pointer;
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    font-family: HarmonyBold;
-    font-size: 15px;
-}
+        h3 {
+            text-align: center;
+            font-family: HarmonyLight
+        }
 
-</style>
+        body {
+            background-color: #DBF9FC;
+        }
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        input[type=submit] {
+            padding: 5px 15px;
+            background: #DBF9FC;
+            border: 1px solid black;
+            cursor: pointer;
+            -webkit-border-radius: 5px;
+            border-radius: 5px;
+            font-family: HarmonyBold;
+            font-size: 15px;
+        }
+    </style>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
 </head>
 
 <body>
 
-<!-- sql: grab food information first -->
-<?php
-include "../db.php";
-db_open();
+    <!-- sql: grab food information first -->
+    <?php
+    include "../db.php";
+    db_open();
 
-$timestamp = $_GET["baseid"];
-$order = db_query("SELECT password, locker, foodname, location FROM food WHERE baseid='$timestamp' AND isdeliver=2;");
-$prows = db_num_rows($order);
-
-if ($prows > 0) {
-    while ($row = $order->fetch_assoc()) {
-        $password = $row['password'];
-        $locker = $row['locker'];
-        $food = $row['foodname'];
-        $location = $row['location'];
+    session_start();
+    if (!isset($_SESSION['loggedin'])) {
+        header('Location: /delivery/login.php');
+        exit;
     }
-} else {
-    echo "<script>location.replace('/delivery.php');</script>";
-}
 
-$rpass = $password + 10000
-?>
+    $username = $_SESSION['username'];
 
+    $timestamp = $_GET["baseid"];
+    $order = db_query("SELECT password, locker, foodname, location FROM food WHERE baseid='$timestamp' AND isdeliver=2;");
+    $prows = db_num_rows($order);
 
-<h1>
-<?php
+    if ($prows > 0) {
+        while ($row = $order->fetch_assoc()) {
+            $password = $row['password'];
+            $locker = $row['locker'];
+            $food = $row['foodname'];
+            $location = $row['location'];
+        }
+    } else {
+        echo "<script>location.replace('/delivery.php');</script>";
+    }
 
-echo "Delivering $food <br> <br>";
-
-?>
-
-<?php
-echo "<img src='https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=$rpass&choe=UTF-8%22%20title=%22your%20order%20is%20ready%22' />"
-?>
-
-</h1>
-
-<h1>
-
-<?php
-echo "This $food goes to locker $locker. <br>";
-?>
-
-<?php
-echo "<br>";
-echo "<a href='http://maps.google.com?q=$location'>"
-?>
-<input type="submit" value="Show location on Google Map" />
-</a>
+    $rpass = $password + 10000
+    ?>
 
 
-</h1>
+    <h1>
+        <?php
+
+        echo "Delivering $food <br> <br>";
+
+        ?>
+
+        <?php
+        echo "<img src='https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=$rpass&choe=UTF-8%22%20title=%22your%20order%20is%20ready%22' />"
+        ?>
+
+    </h1>
+
+    <h1>
+
+        <?php
+        echo "This $food goes to locker $locker. <br>";
+        ?>
+
+        <?php
+        echo "<br>";
+        echo "<a href='http://maps.google.com?q=$location'>"
+        ?>
+        <input type="submit" value="Show location on Google Map" />
+        </a>
+
+
+    </h1>
 
 </body>
+
 </html>
