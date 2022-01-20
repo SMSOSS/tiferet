7c1137dc49d63ebe815954a31f995e5a1a309094
Wiley Lau <henloboii@protonmail.ch>
2022-01-20 12:57:13 +0800

    tiferet: cpanel: add back css styling
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- cpanel/addshop.php
+++ cpanel/addshop.php
@@ -2,6 +2,92 @@
 
 <head>
     <title>Add new restaurant</title>
+    <style>
+        @font-face {
+            font-family: HarmonyBold;
+            src: url('../fonts/bold.ttf');
+        }
+
+        @font-face {
+            font-family: HarmonyReg;
+            src: url('../fonts/regular.ttf');
+        }
+
+        @font-face {
+            font-family: HarmonyLight;
+            src: url('../fonts/light.ttf');
+        }
+
+        h1 {
+            text-align: center;
+            font-family: HarmonyBold;
+        }
+
+        h3 {
+            text-align: center;
+            font-family: HarmonyLight
+        }
+
+        input[type=submit] {
+            padding: 5px 15px;
+            background: #DBF9FC;
+            border: 1px solid black;
+            cursor: pointer;
+            -webkit-border-radius: 5px;
+            border-radius: 5px;
+            font-family: HarmonyBold;
+            font-size: 15px;
+        }
+
+        input[type=text] {
+            padding: 5px 15px;
+            border: 1px solid black;
+            cursor: pointer;
+            -webkit-border-radius: 5px;
+            border-radius: 5px;
+            font-family: HarmonyLight;
+            font-size: 15px;
+        }
+
+        body {
+            background-color: #DBF9FC;
+        }
+
+
+        h2 {
+            text-align: center;
+            font-family: HarmonyReg
+        }
+
+        /* Add a black background color to the top navigation */
+        .topnav {
+            background-color: #40E0D0;
+            overflow: hidden;
+        }
+
+        /* Style the links inside the navigation bar */
+        .topnav a {
+            float: center;
+            color: #000000;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        /* Change the color of links on hover */
+        .topnav a:hover {
+            /* background-color: #ddd;
+  color: black; */
+        }
+
+        /* Add a color to the active/current link */
+        .topnav a.active {
+            background-color: #8F00FF;
+            color: white;
+        }
+    </style>
+
 </head>
 
 <body>
