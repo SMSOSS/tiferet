178cb19baadec761e8eca6209b54a24c6fb44ce6
Wiley Lau <henloboii@protonmail.ch>
2022-02-04 21:51:12 +0800

    tiferet: register: fix registration
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- user/register.php
+++ user/register.php
@@ -277,7 +277,7 @@ if (isset($_SESSION['loggedin'])) {
                 $ucount = db_num_rows($cuname);
                     if ($ecount == 0 && $ucount == 0){
                         db_query("INSERT INTO `userdata`(`username`, `password`, `isdelivery`, `email`) VALUES ('$username','$password','0', '$email')");
-                        header("Location: /home.php")
+                        header("Location: /home.php");
                     } else if ($ecount == 0){
                         echo "<errMsg>Please use another username.</errMsg>";
                     } else {
