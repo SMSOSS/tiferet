31d256683fefd87fb83c9fd24ad8222b0933d5d9
Wiley Lau <henloboii@protonmail.ch>
2022-02-04 12:12:04 +0800

    tiferet: assets: import db and vars
    * taken from 1.0
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

---
+++ assets/vars.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    /* import start */
+    include "/assets/db.php";
+
+    /* variable start */
+    global $baseid;
+    global $dummy;
+    $baseid = strtotime("now");
+    // $locker = 9;
+    $pass = mt_rand(1000, 9999);
+    $foodid = 1;
+    $lcount = 2; // number of lockers
+
+    $version = 1.3;
+    /* real bug trigger. save all data into db */
+    db_open();
+    ?>
+
+</body>
+
+</html>
