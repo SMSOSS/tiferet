9a602fdd97e3564b6a25d73b1e47899ea12fe1b0
Wiley Lau <henloboii@protonmail.ch>
2022-03-24 13:00:14 +0800

    tiferet: init utilities
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

---
+++ assets/utils.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<!-- tiferet utilities HTML.
+     DO not edit unless you know what you're doing.
+     This file contains necessary functions for tiferet. -->
+
+<head>
+    <?php
+        include("/assets/db.php");
+        session_start();
+    ?>
+</head>
